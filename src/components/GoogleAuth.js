65e9df7c9ccbea0b0b05bd1c626dcaa48f93781e
bardrabbit <bardrabbit@gmail.com>
2021-07-13 11:09:31 -0400

    listen to and update our auth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '1026868201166-l016e61ulcmgffom8dom89bandpjikt1.apps.googleusercontent.com',
-        scope: 'email',
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '1026868201166-l016e61ulcmgffom8dom89bandpjikt1.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
   }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I do not know if I am signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>Signed in</div>;
+    } else {
+      return <div>Not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderButton()}</div>;
   }
 }
 
