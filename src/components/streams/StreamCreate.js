3c7f4941f3e5b6717d4d7adf75bf6f0edaa5463c
bardrabbit <bardrabbit@gmail.com>
2021-07-14 12:24:38 -0400

    restyle the form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
-  renderInput({ input }) {
-    return <input {...input} />;
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
   }
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="enter description"
+        />
       </form>
     );
   }
