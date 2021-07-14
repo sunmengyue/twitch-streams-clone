ec0745f2c480894f97c997edf5c357c099488915
bardrabbit <bardrabbit@gmail.com>
2021-07-14 12:17:24 -0400

    add redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import { reducer as formReducer } from 'redux-form';
 
-export default combineReducers({ auth: authReducer });
+export default combineReducers({ auth: authReducer, form: formReducer });
