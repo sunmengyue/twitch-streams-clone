f413f6b886a9cfadef1d1a7239edd1649df15ac1
bardrabbit <bardrabbit@gmail.com>
2021-07-16 05:27:26 -0400

    create a history object and replace BrowserRouter with a plain router

--- src/actions/index.js
+++ src/actions/index.js
@@ -28,6 +28,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
     type: CREATE_STREAM,
     payload: response.data,
   });
+  // do programmatic navigation to get the user back to the root route
 };
 
 export const fetchStreams = () => async (dispatch) => {
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
 
@@ -33,11 +34,24 @@ class StreamList extends Component {
     }
   }
 
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create stream
+          </Link>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
       </div>
     );
   }
@@ -47,6 +61,7 @@ const mapStateToProps = (state) => {
   return {
     streams: Object.values(state.streams),
     currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn,
   };
 };
 
